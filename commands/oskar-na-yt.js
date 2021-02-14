6b45ab9fd5235c2f202d127f2064bcebc0c34306
Oskar Nykiel <oskar_nykiel@outlook.com>
2021-02-14 19:53:29 +0100

    Add command handler + two new commends
    + command handler
    + command "ping"
    + command "oskar-na-yt"

---
+++ commands/oskar-na-yt.js
@@ -0,0 +1,7 @@
+module.exports = {
+    name: 'oskar-na-yt',
+    description: "show oskar's channel on Youtube",
+    execute(message, args){
+        message.channel.send('https://www.youtube.com/channel/UCfpa_cEoWCjDXDshGK3KyiQ');
+    }
+}
--- index.js
+++ index.js
@@ -1,13 +1,35 @@
 const Discord = require('discord.js')
-
 const client = new Discord.Client();
 
+const prefix = 'czaru!';
+
+const fs = require('fs');
+
+client.commands = new Discord.Collection();
+
+const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
+for(const file of commandFiles){
+    const command = require(`./commands/${file}`);
+ 
+    client.commands.set(command.name, command);
+}
 
 
 client.once('ready', () =>{
     console.log('Czarna Mamba jest gotowa do akcji!');
 });
 
+client.on('message', message =>{
+    if(!message.content.startsWith(prefix) || message.author.bot) return;
 
+    const args = message.content.slice(prefix.length).split(/ +/);
+    const command = args.shift().toLowerCase();
+
+    if(command === 'ping'){
+        client.commands.get('ping').execute(message, args);
+    }else if(command === 'oskar-na-yt'){
+        client.commands.get('oskar-na-yt').execute(message, args);
+    }
+});
 
 client.login('');
