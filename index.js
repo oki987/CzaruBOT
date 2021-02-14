36657f4e00dde0c87ce07aaeb4ac8a85bb99b899
Oskar Nykiel <oskar_nykiel@outlook.com>
2021-02-14 14:39:29 +0100

    Create a bot and login message

---
+++ index.js
@@ -0,0 +1,13 @@
+const Discord = require('discord.js')
+
+const client = new Discord.Client();
+
+
+
+client.once('ready', () =>{
+    console.log('Czarna Mamba jest gotowa do akcji!');
+});
+
+
+
+client.login('');
